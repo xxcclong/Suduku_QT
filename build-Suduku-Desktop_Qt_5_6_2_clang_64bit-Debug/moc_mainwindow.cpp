536d37ec5f65ae4f033253ad92b218e21a4b8ed4
xxcclong <641568013@qq.com>
2017-08-28 17:33:10 +0800

    菜单栏 加完了

--- build-Suduku-Desktop_Qt_5_6_2_clang_64bit-Debug/moc_mainwindow.cpp
+++ build-Suduku-Desktop_Qt_5_6_2_clang_64bit-Debug/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[4];
-    char stringdata0[29];
+    QByteArrayData data[10];
+    char stringdata0[60];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,10 +32,17 @@ static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
 QT_MOC_LITERAL(1, 11, 7), // "newgame"
 QT_MOC_LITERAL(2, 19, 0), // ""
-QT_MOC_LITERAL(3, 20, 8) // "readgame"
+QT_MOC_LITERAL(3, 20, 4), // "open"
+QT_MOC_LITERAL(4, 25, 4), // "save"
+QT_MOC_LITERAL(5, 30, 6), // "saveas"
+QT_MOC_LITERAL(6, 37, 5), // "about"
+QT_MOC_LITERAL(7, 43, 4), // "easy"
+QT_MOC_LITERAL(8, 48, 6), // "middle"
+QT_MOC_LITERAL(9, 55, 4) // "hard"
 
     },
-    "MainWindow\0newgame\0\0readgame"
+    "MainWindow\0newgame\0\0open\0save\0saveas\0"
+    "about\0easy\0middle\0hard"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,7 +52,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       8,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -53,10 +60,22 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   24,    2, 0x08 /* Private */,
-       3,    0,   25,    2, 0x08 /* Private */,
+       1,    0,   54,    2, 0x08 /* Private */,
+       3,    0,   55,    2, 0x08 /* Private */,
+       4,    0,   56,    2, 0x08 /* Private */,
+       5,    0,   57,    2, 0x08 /* Private */,
+       6,    0,   58,    2, 0x08 /* Private */,
+       7,    0,   59,    2, 0x08 /* Private */,
+       8,    0,   60,    2, 0x08 /* Private */,
+       9,    0,   61,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Bool,
+    QMetaType::Bool,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -70,11 +89,18 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->newgame(); break;
-        case 1: _t->readgame(); break;
+        case 1: _t->open(); break;
+        case 2: { bool _r = _t->save();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 3: { bool _r = _t->saveas();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 4: _t->about(); break;
+        case 5: _t->easy(); break;
+        case 6: _t->middle(); break;
+        case 7: _t->hard(); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
 const QMetaObject MainWindow::staticMetaObject = {
@@ -102,13 +128,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 8;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 8)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 8;
     }
     return _id;
 }
--- suduku/mainwindow.h
+++ suduku/mainwindow.h
@@ -5,6 +5,7 @@
 #include <QDebug>
 #include <QMenuBar>
 #include <QAction>
+#include <QActionGroup>
 class QAction;
 class Qlabel;
 namespace Ui {
@@ -17,18 +18,36 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    int difficultynum = 1;
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 private slots:
     void newgame();
-    void readgame();
+    void open();
+    bool save();
+    bool saveas();
+    void about();
+    void easy();
+    void middle();
+    void hard();
+
 private:
     Ui::MainWindow *ui;
     void createMenus();
     void createActions();
     QMenu *fileMenu;
+    QMenu *infoMenu;
+    QMenu* difficulty;
+    QActionGroup *difficultyGroup;
+    QAction *easyAction;
+    QAction *middleAction;
+    QAction *hardAction;
     QAction *openAction;
     QAction *newAction;
+    QAction *aboutAction;
+    QAction *saveAction;
+    QAction *saveasAction;
+
 };
 
 #endif // MAINWINDOW_H
